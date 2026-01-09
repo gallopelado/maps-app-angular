b3dbca396a9026401d24dabb4ca5ee498d1977a7
Tío Gallo <juanftp100@gmail.com>
2026-01-09 15:44:00 -0300

    uso de observable y señal en titulo dinamico

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -19,7 +19,16 @@ export class NavbarComponent {
     title: `${route.title ?? 'Maps en Angular'}`
   })).filter(route => route.path !== '**');
 
-  pageTitle$ = toSignal(this.router.events.pipe(
+  // como observable
+  pageTitle$ = this.router.events.pipe(
+    filter( event => event instanceof NavigationEnd ),
+    tap( event => console.log(event) ),
+    map( event => event.url ),
+    map( url => routes.find( route => `/${ route.path }` === url )?.title ?? 'Mapas' )
+  );
+
+  // como señal
+  pageTitle = toSignal(this.router.events.pipe(
     filter( event => event instanceof NavigationEnd ),
     tap( event => console.log(event) ),
     map( event => event.url ),
